888570671a9dc448d72f2249dd0269c439afc876
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-30 17:45:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,8 +26,9 @@ class Transfer
       return @status
     else
       #binding.pry
-      puts "Transaction rejected. Please check your account balance."
       @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+     
     end  
   end  
   
