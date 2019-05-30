15007a6ffe7bf41a4f74b29a39b9f2dc049e032f
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-30 17:40:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount
+  attr_reader :status
+  
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending" 
+  end  
+  
+  def valid?
+    sender.valid? == true && receiver.valid? == true ? true : false
+  end  
+  
+  def execute_transaction
+    if @status == "pending" && self.valid? == true
+      #binding.pry
+      @sender.balance = @sender.balance - amount 
+      @receiver.balance = @receiver.balance + amount
+      @status = "complete"
+    elsif @status == "complete" 
+      #binding.pry
+      return @status
+    else
+      binding.pry
+      return "Transaction rejected. Please check your account balance."
+    end  
+  end  
+  
+  def reverse_transfer
+    #binding.pry
+    
+  end
+    
 end
